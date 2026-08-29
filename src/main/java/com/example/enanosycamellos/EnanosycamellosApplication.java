5d9315fc3a17c51a0ba6bb80867e74296042f88a
chuluet <santiagotoromedina@gmail.com>
2026-08-29 14:54:50 -0500

    project init

---
+++ src/main/java/com/example/enanosycamellos/EnanosycamellosApplication.java
@@ -0,0 +1,13 @@
+package com.example.enanosycamellos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnanosycamellosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnanosycamellosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/enanosycamellos/EnanosycamellosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enanosycamellos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnanosycamellosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
